4d6c285e55529f678f837c3dc73fa838e7ce8ee9
QBeukelman <quentin.nextresearch@gmail.com>
2023-06-22 17:02:08 +0200

    Initial Commit

---
+++ Shapes/ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesApp.swift
+//  Shapes
+//
+//  Created by Quentin Beukelman on 22/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
