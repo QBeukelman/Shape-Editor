ae48c00a753642259182264c1ded66ed1e05e1c6
QBeukelman <quentin.nextresearch@gmail.com>
2023-06-25 12:04:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,62 @@
+
+# de Volksbank
+
+## Shape Editor Application SwiftUI
+
+
+This is a Shape Editor application built using SwiftUI. The application serves as a test assignment for the iOS Developer position at de Volksbank. It allows you to add and manipulate shapes on a canvas.
+
+
+
+## Features
+
+> **Add shapes:** Tap on the canvas to add a new shape at the tapped location. By default, the shape color, size and type will be random.
+
+> **Edit shapes:** When a shape is selected, you can modify its properties using the Edit Shape Menu. The properties you can change include the color, shape type (ellipse or rectangle), and size.
+
+> **Drag shapes:** You can drag shapes on the canvas by tapping and holding on a shape and then moving it to a new location.
+
+> **Clear canvas:** You can clear the canvas and remove all shapes by tapping the eraser button.
+
+
+
+## Usage
+
+1. Run the application.
+2. Tap on the canvas to add a shape at the tapped location. By default, it will be a random shape.
+3. To select a shape and access its properties, tap on the shape.
+4. The Edit Shape Menu will appear, allowing you to modify the selected shape's properties.
+5. You can change the color of the shape by using the color picker.
+6. You can switch the shape type between ellipse and rectangle using the shape toggle.
+7. Adjust the size of the shape using the size slider.
+8. Tap outside the Edit Shape Menu to close it.
+9. To move a shape, tap and hold on it, then drag it to a new location.
+10. To remove all shapes from the canvas, tap the eraser button.
+
+
+
+## Implementation Details
+
+The application is implemented using SwiftUI and consists of the following components:
+
+> **ContentView:** The main view of the application, which contains the canvas, shape rendering, shape manipulation functions, and the Edit Shape Menu.
+
+> **Shape:** A model representing a shape. It contains properties such as position, shape type, size, color, and selection status.
+
+> **ShapeType:** An enumeration representing the shape types (ellipse and rectangle).
+
+> **Extensions:** Additional extensions for generating random colors, random sizes, and previewing the ContentView.
+
+
+
+## Preview
+
+The ContentView is previewed in the ContentView_Previews struct.
+
+```swift
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+```
--- Shapes/ContentView.swift
+++ Shapes/ContentView.swift
@@ -8,14 +8,277 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var shapes: [Shape] = []
+    @State private var selectedShapeType: ShapeType = .ellipse
+    @State var g_tapLocation: CGPoint = .zero
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        renderCanvas()
+        
+        // MARK: - Add shape
+            .onTapGesture { tapLocation in
+                g_tapLocation = tapLocation
+                // Deselect all shapes when a new shape is added
+                shapes.indices.forEach {
+                    shapes[$0].isSelected = false
+                    shapes[$0].shadowColor = Color.clear
+                }
+                let shape = Shape.randomShape(at: tapLocation)
+                shapes.append(shape)
+            }
+    }
+    
+    
+    // MARK: - Edit Shape Menu
+    func renderMenu() -> some View {
+        if let selectedShape = shapes.first(where: { $0.isSelected }) {
+            return AnyView(
+                VStack(spacing: 20) {
+                    
+                    addColorPicker(selectedShape: selectedShape)
+                    addShapeToggle(selectedShape: selectedShape)
+                    addSizeSlider(selectedShape: selectedShape)
+                    
+                    Button("Done") {
+                        shapes.indices.forEach {
+                            shapes[$0].isSelected = false
+                            shapes[$0].shadowColor = Color.clear
+                        }
+                    }
+                    .padding()
+                    .background(Color.gray)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                }
+            )
+        } else {
+            return AnyView(EmptyView())
         }
-        .padding()
+    }
+    
+    // MARK: - Add Color Picker
+    func addColorPicker(selectedShape: Shape) -> some View {
+        ColorPicker("Select Color", selection: Binding(
+            get: { selectedShape.color },
+            set: { newValue in
+                if let index = shapes.firstIndex(where: { $0.id == selectedShape.id }) {
+                    shapes[index].color = newValue
+                    shapes[index].shadowColor = newValue
+                }
+            }
+        ))
+    }
+    
+    // MARK: - Add Shapes Toggle
+    func addShapeToggle(selectedShape: Shape) -> some View {
+        Toggle("Circle", isOn: Binding(
+            get: { selectedShape.shapeType == .ellipse },
+            set: { newValue in
+                if let index = shapes.firstIndex(where: { $0.id == selectedShape.id }) {
+                    if newValue {
+                        shapes[index].shapeType = .ellipse
+                        let shape = Shape.randomShape(
+                            at: g_tapLocation,
+                            shapeType: .ellipse,
+                            size: shapes[index].size,
+                            color: shapes[index].color)
+                        shapes[index] = shape
+                        shapes[index].isSelected = true
+                        shapes[index].shadowColor = shape.color
+                        
+                    } else {
+                        shapes[index].shapeType = .rectangle
+                        let shape = Shape.randomShape(
+                            at: g_tapLocation,
+                            shapeType: .rectangle,
+                            size: shapes[index].size,
+                            color: shapes[index].color)
+                        shapes[index] = shape
+                        shapes[index].isSelected = true
+                        shapes[index].shadowColor = shape.color
+                    }
+                }
+                selectedShapeType = newValue ? .ellipse : .rectangle
+            }
+        ))
+        .tint(.gray)
+    }
+    
+    // MARK: - Add Size Slider
+    func addSizeSlider(selectedShape: Shape) -> some View {
+        HStack {
+            Text("Size")
+                .padding(.trailing, 20)
+            Slider(value: Binding(
+                get: { selectedShape.size.width },
+                set: { newValue in
+                    if let index = shapes.firstIndex(where: { $0.id == selectedShape.id }) {
+                        shapes[index].size = CGSize(width: newValue, height: newValue)
+                    }
+                }
+            ), in: 50...150, step: 1)
+        }
+        .tint(.gray)
+    }
+    
+    
+    // MARK: - Render Shapes
+    func renderShapes() -> some View {
+        ZStack {
+            ForEach(shapes) { shape in
+                shape.view
+                    .frame(width: shape.size.width, height: shape.size.height)
+                    .position(x: shape.position.x, y: shape.position.y)
+                    .foregroundColor(shape.color)
+                    .shadow(color: shape.shadowColor, radius: 15)
+                    .onTapGesture {
+                        handleTap(shape: shape)
+                        g_tapLocation = shape.position
+                    }
+                
+                // MARK: Drag
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                if let index = shapes.firstIndex(where: { $0.id == shape.id }) {
+                                    shapes[index].position = value.location
+                                    shapes[index].isDragging = true
+                                    g_tapLocation = value.location
+                                }
+                            })
+                        
+                            .onEnded({ value in
+                                if let index = shapes.firstIndex(where: { $0.id == shape.id }) {
+                                    shapes[index].isDragging = false
+                                    g_tapLocation = value.location
+                                }
+                            })
+                    )
+            }
+        }
+    }
+    
+    
+    // MARK: - Render Canvas
+    func renderCanvas() -> some View {
+        ZStack {
+            Color.white
+                .edgesIgnoringSafeArea(.all)
+            
+            // MARK: Render Shapes
+            renderShapes()
+            
+            VStack {
+                // MARK: Clear Button
+                HStack {
+                    Spacer()
+                    Button(action: { shapes.removeAll() }) {
+                        Image(systemName: "eraser.fill")
+                            .font(.title)
+                            .foregroundColor(.gray)
+                    }
+                    .padding(10)
+                }
+                Spacer()
+                
+                // MARK: Render Edit Menu
+                HStack {
+                    Spacer()
+                    renderMenu()
+                }
+                .onTapGesture {}
+                .padding()
+                .background(
+                    Color.white
+                        .edgesIgnoringSafeArea(.all)
+                )
+            }
+        }
+    }
+    
+    
+    
+    // MARK: - Handle Tap
+    func handleTap(shape: Shape) {
+        // Deselect all shapes when a new shape is added
+        shapes.indices.forEach {
+            shapes[$0].isSelected = false
+            shapes[$0].shadowColor = Color.clear
+        }
+        if let index = shapes.firstIndex(where: { $0.id == shape.id }) {
+            if shapes[index].isSelected {
+                shapes[index].shadowColor = Color.clear
+                shapes[index].isSelected = false
+            } else {
+                // Deselect other shapes
+                shapes.indices.forEach { shapes[$0].isSelected = false }
+                shapes[index].shadowColor = shape.color
+                shapes[index].isSelected = true
+            }
+        }
+    }
+}
+
+
+// MARK: - Shape
+struct Shape: Identifiable {
+    var isDragging = false
+    var position = CGPoint.zero
+    let id = UUID()
+    let view: AnyView
+    var shapeType: ShapeType
+    var size: CGSize
+    var color: Color
+    var shadowColor: Color = Color.clear
+    var isSelected = false
+    
+    static func randomShape(at position: CGPoint, shapeType: ShapeType? = nil, size: CGSize? = nil, color: Color? = nil) -> Shape {
+        let randomColor = color ?? Color.random
+        let randomSize = CGFloat.random(in: 50...150)
+        let equalRandomSize = size ?? CGSize(width: randomSize, height: randomSize)
+        let selectedShapeType = shapeType ?? (Bool.random() ? ShapeType.ellipse : ShapeType.rectangle)
+        
+        let shapeView: AnyView
+        switch selectedShapeType {
+        case .ellipse:
+            shapeView = AnyView(Ellipse())
+        case .rectangle:
+            shapeView = AnyView(Rectangle())
+        }
+        
+        return Shape(
+            isDragging: false,
+            position: position,
+            view: shapeView,
+            shapeType: selectedShapeType,
+            size: equalRandomSize,
+            color: randomColor,
+            shadowColor: Color.clear
+        )
+    }
+}
+
+
+enum ShapeType {
+    case ellipse
+    case rectangle
+}
+
+extension Color {
+    static var random: Color {
+        let red = Double.random(in: 0...1)
+        let green = Double.random(in: 0...1)
+        let blue = Double.random(in: 0...1)
+        return Color(red: red, green: green, blue: blue)
+    }
+}
+
+extension CGFloat {
+    static var random: CGFloat {
+        return CGFloat(Float.random(in: 0...1))
     }
 }
 
